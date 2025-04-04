42ebfd86e02e533555738ab8205e4d610430f7ed
EhsanHosseini93 <e.alihosseini1993@gmail.com>
2025-04-04 18:28:39 +0330

    Add files via upload

---
+++ config.js
@@ -0,0 +1,39 @@
+export const config = {
+    storeName: "TehranBook",
+    featuredBooks: [
+        {
+            title: "کیمیاگر",
+            author: "Paulo Coelho",
+            translator: "Roohollah Hossein Nejad",
+            price: 24.99,
+            description: "The Persian edition of The Alchemist",
+            image: "/5998845187159345154.jpg"
+        },
+        {
+            title: "Persian Book",
+            author: "Unknown",
+            price: 29.99,
+            description: "A classic Persian literature piece with vintage styling",
+            image: "/5998845187159345155.jpg"
+        },
+        {
+            title: "Modern Persian Literature",
+            author: "Unknown",
+            price: 27.99,
+            description: "A contemporary Persian book featuring artistic collage design",
+            image: "/5999077841242802342.jpg"
+        }
+    ],
+    contactEmail: "info@bookhaven.com",
+    socialMedia: {
+        twitter: "bookhaven",
+        facebook: "bookhaven",
+        instagram: "bookhaven"
+    },
+    theme: {
+        primary: "#8B4513",    // Saddle Brown
+        secondary: "#D2691E",  // Chocolate
+        text: "#3E2723",      // Dark Brown
+        background: "#FFF8F3"  // Off-White
+    }
+};
